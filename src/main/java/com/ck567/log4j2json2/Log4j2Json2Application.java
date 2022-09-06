c1c1b422e5882c30dd01af374ba0763262291bd5
Hao.Wu <7923324+ck_567@user.noreply.gitee.com>
2022-09-06 14:09:57 +0800

    main and test

---
+++ src/main/java/com/ck567/log4j2json2/Log4j2Json2Application.java
@@ -0,0 +1,13 @@
+package com.ck567.log4j2json2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Log4j2Json2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Log4j2Json2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ck567/log4j2json2/Log4j2Json2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ck567.log4j2json2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Log4j2Json2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
